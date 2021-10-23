6c1731ca9f87c3c8de3f68cb7aa8b65ce977bcb9
Sunil Kumar <ssunil.uk@gmail.com>
2021-10-23 15:05:32 +0530

    make it better

--- src/controllers/park.controller.js
+++ src/controllers/park.controller.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const router = express.Router();
 require("dotenv").config()
 
-const num = process.env.SLOT
+const num = process.env.SLOT || 5;
 
 const Park = require ("../models/Park.model")
 const Slot = require ("../models/Slot.model")
--- src/models/park.model.js
+++ src/models/park.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 
-const number = process.env.SLOT
+const number = process.env.SLOT || 5;
 
 const parkSchema = new mongoose.Schema({
     car: {type: String, required: true},
